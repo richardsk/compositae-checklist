4e30f9b164907cbb655701cf935bbd9bd2e23f45
richardsk777@gmail.com <richardsk777@gmail.com@8761fe79-0b2e-d464-87d3-e9a6d1f75ba7>
2014-06-24 01:51:29 +0000

    GCC2

---
+++ GCC2/GCCWebService/Controllers/TreeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+
+namespace GCCWebService.Controllers
+{
+    public class TreeNode
+    {
+        public string id { get; set; }
+        public string label { get; set; }
+        public bool inode { get; set; }
+        public bool open { get; set; }
+
+    }
+
+    public class TreeController : ApiController
+    {
+        //
+        // GET: /Tree/
+        
+        public List<TreeNode> Get()
+        {
+            return new List<TreeNode> { new TreeNode { id = "1", label = "Compositae", inode=true, open= false } };
+            //return new { id = 1, label = "Compositae" };
+            //return new JsonResult { Data = "[{\"id\":\"1\",\"label\":\"Compositae\",\"inode\":true,\"open\":false,\"branch\":[{\"id\":\"2\",\"label\":\"Eupatorium\",\"inode\":true}]}]" };
+        }
+
+    }
+}
